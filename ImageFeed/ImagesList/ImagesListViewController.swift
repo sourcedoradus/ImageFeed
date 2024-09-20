278352710b4d9dbf73bbf8197a397321bf41eb83
sdoradus <valentine.medvedev@gmail.com>
2024-09-20 14:11:01 -0300

    refactor: add didSet to image in SingleImageViewController

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -38,8 +38,6 @@ final class ImagesListViewController: UIViewController {
             }
             
             let image = UIImage(named: photosName[indexPath.row])
-        //    _ = viewController.view
-       //     viewController.imageView.image = image
             viewController.image = image
         } else {
             super.prepare(for: segue, sender: sender)
