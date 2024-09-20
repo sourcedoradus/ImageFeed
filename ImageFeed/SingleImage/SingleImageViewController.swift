4ed00fc202c17b6273476bc32bd502f4c8093960
sdoradus <valentine.medvedev@gmail.com>
2024-09-20 16:48:09 -0300

    feat: add present view controller

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -18,6 +18,10 @@ final class SingleImageViewController: UIViewController {
     
     @IBOutlet private var imageView: UIImageView!
     
+    @IBAction private func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
