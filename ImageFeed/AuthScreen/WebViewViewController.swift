3dce4d16dfdaa3db6674802c4c2c94051414a611
sdoradus <valentine.medvedev@gmail.com>
2024-12-30 09:44:56 -0300

    refactor: remove implicit unwraps in URLComponents

--- ImageFeed/AuthScreen/WebViewViewController.swift
+++ ImageFeed/AuthScreen/WebViewViewController.swift
@@ -19,20 +19,30 @@ final class WebViewViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString)!
+        loadAuthView()
+    }
+    
+    enum WebViewConstants {
+        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    }
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+            return
+        }
+        
         urlComponents.queryItems = [
             URLQueryItem(name: "client_id", value: Constants.accessKey),
             URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
             URLQueryItem(name: "response_type", value: "code"),
             URLQueryItem(name: "scope", value: Constants.accessScope)
         ]
-        let url = urlComponents.url!
+        
+        guard let url = urlComponents.url else {
+            return
+        }
         
         let request = URLRequest(url: url)
         webView.load(request)
-    }
-    
-    enum WebViewConstants {
-        static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
     }
 }
